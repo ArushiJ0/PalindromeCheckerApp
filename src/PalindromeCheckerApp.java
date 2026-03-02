15d1e397719a20efe642feb6dcbd84a201e83d1e
ArushiJ0 <aj2990@srmist.edu.in>
2026-03-02 09:31:22 +0530

    UC6

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,16 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "noon";
+        String input = "civic";
         System.out.println("Input :" + input );
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
         for (char c : input.toCharArray()) {
-                stack.push(c);
+            queue.add(c);
+            stack.push(c);
             }
             boolean isPalindrome = true;
 
-            for (char c : input.toCharArray()) {
-                if (c != stack.pop()) {
+          while(!queue.isEmpty()){
+              if (queue.remove() != stack.pop()) {
                     isPalindrome = false;
                     break;
                 }
@@ -19,3 +23,15 @@ public class PalindromeCheckerApp {
             System.out.println("Is Palindrome? : " + isPalindrome);
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
