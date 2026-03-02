ee479b773c4c0745c556da06fca2d56371871b57
ArushiJ0 <arushi.123253@gmail.com>
2026-03-02 09:59:36 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,19 +1,7 @@
-import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Input :");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-String reversed = "";
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed += input.charAt(i);
-        }
-            if(input.equals(reversed)){
-                System.out.println(input+ " is a palindrome.");
-            } else {
-                System.out.println(input + " is not a palindrome.");
-            }
-
-        }
-
-    }
+    System.out.println("Welcome to the Palindrome Checker Managemnet System");
+    System.out.println("Version : 1.0");
+    System.out.println("System initialized successfully");
+}
+}
